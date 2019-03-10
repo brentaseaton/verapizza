9a6c1302f8a51b9b125751b3af14b1e0b8a609b2
Brent Seaton <brentaseaton@gmail.com>
2019-03-10 14:10:04 -0400

    navbar v1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-toolbar fixed flat dark prominent class="transparent">
+      <v-img src="exlogo.png" max-height="100" max-width="250" class="mt-4 ml-4 pa-4"></v-img>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items v-for="item in menuItems" :key="item.text" class="hidden-sm-and-down">
+        <v-btn flat>{{ item.text }}</v-btn>
+      </v-toolbar-items>
+
+      <v-menu offset-y class="hidden-md-and-up">
+        <v-btn flat slot="activator">
+          <v-icon left>expand_more</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        <v-list>
+          <v-list-tile v-for="item in menuItems" :key="item.text">
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+
+    <v-carousel hide-controls hide-delimiters height="800">
+      <v-carousel-item src="pizza_ph.jpg"></v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      menuItems: [
+        {
+          text: 'Home'
+        },
+        {
+          text: 'Menu'
+        },
+        {
+          text: 'About'
+        },
+        {
+          text: 'Contact'
+        }
+      ]
+    }
   }
 }
 </script>
